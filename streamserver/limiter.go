28816e3bce6b79e6783a930109736b0d2689eec1
zli0111 <zhengrongli1003@gmail.com>
2021-08-09 16:59:30 -0700

    stream video complete

---
+++ streamserver/limiter.go
@@ -0,0 +1,34 @@
+// use token bucket to make the read limit
+// https://en.wikipedia.org/wiki/Token_bucket
+package main
+
+import (
+	"log"
+)
+
+type ConnLimiter struct {
+	concurrentConn int
+	bucket         chan int
+}
+
+func NewConnLimiter(cc int) *ConnLimiter {
+	return &ConnLimiter{
+		concurrentConn: cc,
+		bucket:         make(chan int, cc),
+	}
+}
+
+func (cl *ConnLimiter) GetConn() bool {
+	if len(cl.bucket) >= cl.concurrentConn {
+		log.Printf("Reached the rate limitation.")
+		return false
+	}
+
+	cl.bucket <- 1
+	return true
+}
+
+func (cl *ConnLimiter) ReleaseConn() {
+	c := <-cl.bucket
+	log.Printf("New connction coming: %d", c)
+}
